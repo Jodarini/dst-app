d005abb1c3e35d5e45aaa2fd31c0016772732735
Jose <rivasjose9428@gmail.com>
2023-09-04 16:18:53 -0500

    Showing items

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,15 @@
 import { gql } from "graphql-request";
 import Image from "next/image";
 
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 const GET_RECIPES = gql`
   query allRecipes {
     crockpotRecipes {
@@ -20,8 +29,6 @@ const GET_RECIPES = gql`
   }
 `;
 
-async function getData() {}
-
 export default async function Home() {
   const { data } = await fetch("https://dont-starve-together-api.xyz/graphql", {
     method: "POST",
@@ -39,20 +46,31 @@ export default async function Home() {
   console.log({ recipes });
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      hello
-      <ul>
-        {recipes.map((recipe) => (
-          <li key={recipe.name}>
-            <Image
-              src={recipe.asset}
-              alt={recipe.name}
-              width={200}
-              height={200}
-            />
-          </li>
-        ))}
-      </ul>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-slate-800">
+      <article>
+        <ul className="grid grid-cols-4">
+          {recipes.map((recipe) => (
+            <li key={recipe.name} className="max-h-[204px]">
+              <Card>
+                <CardHeader className="text-center">
+                  <CardTitle>{recipe.name}</CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <Image
+                    className="m-auto"
+                    src={recipe.asset}
+                    alt={recipe.name}
+                    width={50}
+                    height={50}
+                  />
+                  <p>{recipe.cookingTime}</p>
+                </CardContent>
+                <CardFooter>{recipe.sideEffect}</CardFooter>
+              </Card>
+            </li>
+          ))}
+        </ul>
+      </article>
     </main>
   );
 }
